4a4732feec196981adbcbd8a3cb68534a9657908
David Hibble <139304207+hibbleD@users.noreply.github.com>
2023-10-04 21:22:14 +0000

    Finished code

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -6,6 +6,69 @@ class DirectorsController < ApplicationController
     render({ :template => "director_templates/index" })
   end
 
+  def update
+    #Get the ID out of params
+    d_id = params.fetch("the_id")
+
+    #Look up the existing record
+
+    matching_records = Director.where({:id => d_id})
+
+    the_director = matching_records.at(0)
+    #Overwrite each column with the values from user inputs
+
+    the_director.name = params.fetch("the_name")
+    the_director.dob = params.fetch("the_dob")
+    the_director.bio = params.fetch("the_bio")
+    the_director.image = params.fetch("the_image")
+
+    
+    #Save
+    the_director.save
+    #Redirect to movie details page
+
+    redirect_to("/directors/#{the_director.id}")
+  end
+
+  def create
+    # Retrieve the user's inputs from params
+
+    d = Director.new
+
+    d.name = params.fetch("the_name")
+    d.dob = params.fetch("the_dob")
+    d.bio = params.fetch("the_bio")
+    d.image = params.fetch("the_image")
+
+    d.save
+
+    redirect_to("/directors")
+
+
+    # Create a record in the movie table
+    # Populate each colum with the user input
+    # Save
+
+    # Redirect the user back to the /movies URL
+
+
+
+  end
+
+  def destroy
+    the_id = params.fetch("an_id")
+
+    matching_records = Director.where({:id => the_id})
+
+    the_director = matching_records.at(0)
+
+    the_director.destroy
+
+    redirect_to("/directors")
+
+  end
+
+
   def show
     the_id = params.fetch("path_id")
 
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,32 @@
 class MoviesController < ApplicationController
 
+  def update
+    #Get the ID out of params
+    m_id = params.fetch("the_id")
+
+    #Look up the existing record
+
+    matching_records = Movie.where({:id => m_id})
+
+    the_movie = matching_records.at(0)
+    #Overwrite each column with the values from user inputs
+
+    the_movie.title = params.fetch("the_title")
+    the_movie.year = params.fetch("the_year")
+    the_movie.duration = params.fetch("the_duration")
+    the_movie.description = params.fetch("the_description")
+    the_movie.image = params.fetch("the_image")
+    the_movie.director_id = params.fetch("the_director_id")
+
+    
+    #Save
+    the_movie.save
+    #Redirect to movie details page
+
+    redirect_to("/movies/#{the_movie.id}")
+  end
+  
+  
   def create
     # Retrieve the user's inputs from params
 
